849b2e027f1cd8bcd4481a6cdf8baa3cee0004d8
Joscha <joscha@plugh.de>
2022-01-18 23:46:17 +0100

    nun erfülle meinen Willen!

---
+++ exec/CsePhi.java
@@ -0,0 +1,13 @@
+class CsePhi {
+        public static void main(String[] args) {
+                int i = 0;
+                int j = 0;
+                int z = 0;
+                while (i < 5) {
+                        i = i + 1;
+                        j = j + 1;
+                        z = i + j;
+                }
+                System.out.println(z);
+        }
+}
